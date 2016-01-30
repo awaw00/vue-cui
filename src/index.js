e4abea023c8293b05739e9081f541a1a950f92cb
awaw00 <awaw0618@outlook.com>
2016-01-30 17:26:48 +0800

    remove outdate require

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   dialog: require('./components/Dialog'),
   dialogConfirm: require('./components/DialogConfirm'),
-  dialogOk: require('./components/DialogOk'),
   drawer: require('./components/Drawer'),
   progressBar: require('./components/ProgressBar'),
   selectInput: require('./components/SelectInput'),
