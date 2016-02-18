de142ca66b007890044eaf6c0f26054158045755
aaron <aaron-wang@outlook.com>
2016-02-18 21:25:01 +0800

    dialog transition

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,5 +1,11 @@
 <style lang='less'>
 .dialog-container{
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 1000;
   .dialog-background{
     position: fixed;
     left: 0;
@@ -13,18 +19,20 @@
     padding: 16px;
     background-color: white;
     z-index: 1002;
-    position: fixed;
-    left: 50%;
-    top: 50%;
-    transform: translateX(-50%) translateY(-50%);
+    position: relative;
+    margin: 0 auto;
+    width: 360px;
+    top: 160px;
+    top: 20vh;
   }
 }
 </style>
 <template lang='jade'>
-div.dialog-container
+div.dialog-container(v-show='show', transition='fade')
   div.dialog-background(v-show='show')
-  div.dialog-body.whiteframe-z4(v-show='show', transition='bubble')
+  div.dialog-body.whiteframe-z4(v-show='show', transition='dialog')
     slot
+    div.clearfix
 </template>
 <script>
 import './styles/transition.less'
